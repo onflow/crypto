676d89af32a76dc7086798743c304be9d894be41
Tarak Ben Youssef <tarak.benyoussef@flowfoundation.org>
2025-06-27 18:45:41 +0800

    update DL to FF

--- bls_thresholdsign_core.c
+++ bls_thresholdsign_core.c
@@ -1,7 +1,7 @@
 /*
  * Flow Crypto
  *
- * Copyright Dapper Labs, Inc.
+ * Copyright Flow Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
